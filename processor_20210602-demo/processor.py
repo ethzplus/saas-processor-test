70272df335eb54840a5b21068b12cfa54287c160
heiko <heiko.aydt@gmail.com>
2021-05-24 16:18:15 +0800

    Add status updates and exception handling

--- processor_20210602-demo/processor.py
+++ processor_20210602-demo/processor.py
@@ -1,3 +1,4 @@
+import traceback
 import logging
 import os
 
@@ -5,29 +6,39 @@ logger = logging.getLogger('testing.20210602-demo')
 
 
 def function(working_directory):
-    in_path = os.path.join(working_directory, 'confidential')
-    with open(in_path, 'r') as f:
-        # read the header
-        header = f.readline()
-        print(header)
+    status_path = os.path.join(working_directory, 'status.log')
+    with open(status_path, 'w') as status:
+        try:
+            in_path = os.path.join(working_directory, 'confidential')
+            with open(in_path, 'r') as f:
+                # read the header
+                header = f.readline()
+                status.write(f"{header}\n")
 
-        result = []
-        for t in range(24):
-            line = f.readline()
-            print(line)
+                result = []
+                for t in range(24):
+                    line = f.readline()
+                    print(line)
 
-            line = line.split(',')
-            s = 0
-            for i in range(1, len(line)):
-                s += float(line[i])
+                    line = line.split(',')
+                    s = 0
+                    for i in range(1, len(line)):
+                        s += float(line[i])
 
-            result.append(s)
+                    result.append(s)
 
-    print(f"result: {result}")
+            status.write(f"{result}\n")
 
-    out_path = os.path.join(working_directory, 'aggregated')
-    with open(out_path, 'w') as f:
-        f.write("Time,Aggregated\n")
+            out_path = os.path.join(working_directory, 'aggregated')
+            status.write(f"out_path={out_path}\n")
+            with open(out_path, 'w') as f:
+                f.write("Time,Aggregated\n")
 
-        for t in range(24):
-            f.write(f"{t},{result[t]}\n")
+                for t in range(24):
+                    f.write(f"{t},{result[t]}\n")
+
+            status.write(f"done\n")
+
+        except Exception as e:
+            trace = ''.join(traceback.format_exception(None, e, e.__traceback__))
+            status.write(trace)

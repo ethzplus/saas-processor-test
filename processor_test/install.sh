11c700bd3cc99ad97ffd7f4b1d764f3c28d556b3
heiko <heiko.aydt@gmail.com>
2021-08-01 11:31:07 +0800

    Update install.sh

--- processor_test/install.sh
+++ processor_test/install.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" == "default" ]; then
 	echo "Create python virtual environment"
-	python3 -m venv venv
+	python3 -m venv ./venv
 
 	echo "Activate virtual environment"
-	source venv/bin/activate
+	source ./venv/bin/activate
 
 	echo "Install python dependencies"
-	python3 -m pip install -r requirements.txt
+	python3 -m pip install -r ./requirements.txt
 
 	exit 0
 else

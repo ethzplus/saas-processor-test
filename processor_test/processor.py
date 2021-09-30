7a879286865a258c3331f68aa0e3b6a6064e21e2
heiko <heiko.aydt@gmail.com>
2021-09-30 20:27:23 +0800

    Update processor.py

--- processor_test/processor.py
+++ processor_test/processor.py
@@ -2,32 +2,45 @@ import json
 import logging
 import os
 import sys
+import time
 
 logger = logging.getLogger('test-proc')
 
 
 def function(working_directory):
+    print(f"trigger:progress:0")
+
     a_path = os.path.join(working_directory, 'a')
     with open(a_path, 'r') as f:
         a = json.load(f)
         a = a['v']
+    print(f"a={a}")
+    print(f"trigger:progress:20")
+    time.sleep(0.2)
 
     b_path = os.path.join(working_directory, 'b')
     with open(b_path, 'r') as f:
         b = json.load(f)
         b = b['v']
-
-    print(f"a={a}")
     print(f"b={b}")
+    print(f"trigger:progress:40")
+    time.sleep(0.2)
+
     c = {
         'v': a + b
     }
     print(f"c={c}")
+    print(f"trigger:progress:60")
+    time.sleep(0.2)
 
     c_path = os.path.join(working_directory, 'c')
-
     with open(c_path, 'w') as f:
         json.dump(c, f, indent=4, sort_keys=True)
+    print(f"trigger:progress:80")
+    print(f"trigger:output:c")
+    time.sleep(0.2)
+
+    print(f"trigger:progress:100")
 
 
 if __name__ == '__main__':

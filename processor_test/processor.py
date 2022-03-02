194ae5bf06990a8829e8cf82f572eb1824f6e20e
Orencio Robaina <rorencio@ethz.ch>
2022-03-02 18:05:21 +0100

    testing GeoJSON feature counter

--- processor_test/processor.py
+++ processor_test/processor.py
@@ -6,42 +6,18 @@ import time
 
 logger = logging.getLogger('test-proc')
 
-
-def function(working_directory):
-    print(f"trigger:progress:0")
-
-    a_path = os.path.join(working_directory, 'a')
-    with open(a_path, 'r') as f:
+def feature_count(working_directory):
+    with open(os.path.join(working_directory, 'a'), 'r') as f:
         a = json.load(f)
-        a = a['v']
-    print(f"a={a}")
-    print(f"trigger:progress:20")
-    time.sleep(0.2)
+        count = len(a["features"])
+        print(count)
 
-    b_path = os.path.join(working_directory, 'b')
-    with open(b_path, 'r') as f:
-        b = json.load(f)
-        b = b['v']
-    print(f"b={b}")
-    print(f"trigger:progress:40")
-    time.sleep(0.2)
-
-    c = {
-        'v': a + b
+    b = {
+        'count': count
     }
-    print(f"c={c}")
-    print(f"trigger:progress:60")
-    time.sleep(0.2)
 
-    c_path = os.path.join(working_directory, 'c')
-    with open(c_path, 'w') as f:
-        json.dump(c, f, indent=4, sort_keys=True)
-    print(f"trigger:progress:80")
-    print(f"trigger:output:c")
-    time.sleep(0.2)
-
-    print(f"trigger:progress:100")
-
+    with open(os.path.join(working_directory, 'b'), 'w') as f:
+        json.dump(b, f, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
-    function(sys.argv[1])
+    feature_count(sys.argv[1])

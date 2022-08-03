b71061ec955dd44ca7074eaf17ee52ecc6f50179
Orencio Robaina <rorencio@ethz.ch>
2022-08-03 16:19:43 +0200

    Fixed descriptor.json and using only default configuration

--- processor_test/install.sh
+++ processor_test/install.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
 if [ "$1" == "default" ]; then
-	echo "Create python virtual environment"
-	python3 -m venv ./venv
-
-	echo "Activate virtual environment"
-	source ./venv/bin/activate
-
-	echo "Install python dependencies"
-	python3 -m pip install -r ./requirements.txt
-
-	exit 0
-
-elif [ "$1" == "nscc" ]; then
-	echo "Load python module"
-	module load python/3.8.3
-
-	echo "Create python virtual environment"
-	python3 -m venv ./venv
-
-	echo "Activate virtual environment"
-	source ./venv/bin/activate
-
-	echo "Install python dependencies"
-	python3 -m pip install -r ./requirements.txt
-
-	exit 0
-
-elif [ "$1" == "R" ]; then
 	echo "Install R dependencies"
 	Rscript ./install_dep.R
 

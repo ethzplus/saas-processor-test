9f7d40c47b3379c706b5356be835fe86d57f2c5b
Orencio Robaina <rorencio@ethz.ch>
2022-09-12 16:07:31 +0200

    List ~/R_libs

--- CheckDepsAirQuality/MainScript.R
+++ CheckDepsAirQuality/MainScript.R
@@ -1,20 +1,10 @@
 output_path <- file.path(args[1],"report")
-sink(output_path)
 
 noquote("trigger:progress:0")
 
-# library(raster, lib="~/R_libs/")
-# library(rgdal, lib="~/R_libs/")
-# library(sp, lib="~/R_libs/")
-# library(igraph, lib="~/R_libs/")
-# library(gdistance, lib="~/R_libs/")
-# library(rgeos, lib="~/R_libs/")
-require(raster, lib="~/R_libs/")
-require(rgdal, lib="~/R_libs/")
-require(sp, lib="~/R_libs/")
-require(igraph, lib="~/R_libs/")
-require(gdistance, lib="~/R_libs/")
-require(rgeos, lib="~/R_libs/")
+sink(output_path)
+
+list.dirs("~/R_libs/", recursive=FALSE)
 
 sink()
 
